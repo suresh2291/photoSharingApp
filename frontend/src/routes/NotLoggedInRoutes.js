cf15f0d2c6cabb971df5961354694ff9d88ac84e
suresh2291 <sureshamrutham91@gmail.com>
2023-03-08 21:03:42 +0530

    Front End Protect Routes.

---
+++ frontend/src/routes/NotLoggedInRoutes.js
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function NotLoggedInRoutes() {
+    const { user } = useSelector((state)=>({ ...state }))
+    return user ? <Navigate to="/" /> : <Outlet />
+}
